0a9bf7d12e92ee2833e7f63bae5a0e52b1aa24d3
Farhan933 <farhanfarabiemon@gmail.com>
2023-11-11 19:00:57 +0600

    common use

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
@@ -27,15 +27,55 @@ async function run() {
 
         const countryCollection = client.db('dream11').collection('countries');
         const playersCollection = client.db('dream11').collection('players');
+        const mySquadCollection = client.db('dream11').collection('mySquad');
 
-
+        /* {---------------Countries API-------------} */
         //get all the country api
-        app.get('/country', async(req, res) => {
+        app.get('/country', async (req, res) => {
             const result = await countryCollection.find().toArray();
             res.send(result);
         })
 
-        
+        //get all the players of specific country api
+        app.get('/player/:country', async (req, res) => {
+            const country = req.params.country;
+            const query = { country: country };
+            const result = await playersCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        /* {---------------Players API-------------} */
+        //get specific player details api
+        app.get('/player-details/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) };
+            const result = await playersCollection.findOne(query);
+            res.send(result);
+        })
+
+        /* {---------------mySquad API-------------} */
+        //get all player from mySquad collection
+        app.get('/mySquad', async (req, res) => {
+            const result = await mySquadCollection.find().toArray();
+            res.send(result);
+        })
+
+        //add specific player information with user email
+        app.post('/mySquad', async (req, res) => {
+            const mySquad = req.body;
+            const result = await mySquadCollection.insertOne(mySquad);
+            res.send(result);
+        })
+
+        //delete a specific player from mySquad Collection
+        app.delete('/mySquad/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await mySquadCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
